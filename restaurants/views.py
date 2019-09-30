33949fa7778aa5a53fcef82446e6e04862c62772
barak <balmeshaan@gmail.com>
2019-09-30 20:57:53 +0300

    add task_03

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,20 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    	'my_list': [
+    		{
+    			'restaurant_name':'food inc',
+    			'food_type':'general'
+    		},
+    		{
+    			'restaurant_name':'banana republic',
+    			'food_type':'vegan'
+    		},
+    		{
+    			'restaurant_name':'meat',
+    			'food_type':'carnivore diet'
+    		}
+    	]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +27,11 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+    	'my_object': 
+			{	
+				'restaurant_name':'meat',
+				'food_type':'carnivore diet'
+			}
+    	
     }
     return render(request, 'detail.html', context)
